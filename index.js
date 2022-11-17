78fcd4e91e7e01f884e7ed83c18413c0b13d9621
sajid365-sr <sajidsorker86@gmail.com>
2022-11-17 21:17:47 +0600

    mongoDB setup added

--- index.js
+++ index.js
@@ -2,12 +2,48 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+// MongoDB Connection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.90qadcl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async() =>{
+    try{
+        const serviceCollection = client.db("Clinox").collection('Services');
+        
+
+
+    }
+    catch{
+
+    }
+
+}
+
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Default Root
 app.get('/', (req, res) =>{
@@ -17,6 +53,11 @@ app.get('/', (req, res) =>{
 // All services
 app.get('/services', (req, res) =>{
 
+})
+
+// JW Token
+app.get('/jwt', (req,res) =>{
+    res.send(JSON.stringify(process.env.JW_TOKEN));
 })
 
 app.listen(port, () =>{
