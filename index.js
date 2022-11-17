9dad19a40b4a0c07b8e005557135d00724aa258f
sajid365-sr <sajidsorker86@gmail.com>
2022-11-17 20:38:10 +0600

    jwt token initial setup complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -7,3 +8,17 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Default Root
+app.get('/', (req, res) =>{
+    res.send('Server is Running');
+});
+
+// All services
+app.get('/services', (req, res) =>{
+
+})
+
+app.listen(port, () =>{
+    console.log('Server is running on port: ', port);
+});
