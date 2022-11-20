e272d5468daaa80fb11fdbb711d8929221f27ce3
sajid365-sr <sajidsorker86@gmail.com>
2022-11-20 21:50:43 +0600

    specific review deleted  functin done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -19,6 +19,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 const run = async() =>{
     try{
         const serviceCollection = client.db("Clinox").collection('Services');
+        const reviewCollection = client.db('Clinox').collection('Reviews');
 
         // All services
     app.get('/services', async(req, res) =>{
@@ -28,6 +29,62 @@ const run = async() =>{
 
         res.send(result);
     
+    })
+
+    // service highlight (only 3 service)
+    app.get('/servicesH', async(req, res) =>{
+        const serviceHighlight = JSON.parse(req.query.dataCount);
+        const cursor = serviceCollection.find({});
+        const result = await cursor.limit(serviceHighlight).toArray();
+
+        res.send(result)
+    })
+
+    // Get specific service by id
+    app.get('/services/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const service = await serviceCollection.findOne(query);
+
+        res.send(service);
+    })
+
+    // Add Review
+    app.post('/reviews', async(req, res) =>{
+        const reviews = req.body;
+        const result = await reviewCollection.insertOne(reviews);
+        
+        res.send(result);
+    })
+
+    // Find all review by specific service id
+    app.get('/reviews/:id', async(req,res) =>{
+        const serviceId = req.params.id;
+        const query = {id:serviceId};
+        const cursor = reviewCollection.find(query);
+        const result = await cursor.toArray();
+
+        res.send(result);
+    })
+
+    // Find all review by specific email
+    app.post('/userEmail', async(req, res) =>{
+        const userEmail = req.body.email;
+        const query = {email:userEmail};
+        const cursor = reviewCollection.find(query);
+        const result = await cursor.toArray();
+
+        res.send(result);
+
+    })
+
+    // Delete review by specific id
+    app.delete('/deleteReview/:id', async(req, res) =>{
+        const reviewId = req.params.id;
+        const query = {_id:ObjectId(reviewId)};
+        const result = await reviewCollection.deleteOne(query);
+
+        res.send(result);
     })
 
     }
